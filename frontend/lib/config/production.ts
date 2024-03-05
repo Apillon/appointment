79898dcd33c5fc174f346b538d0ae247177f059d
Tine Mlakar <tine.mlakar@kalmia.si>
2024-03-05 16:07:59 +0100

    production contract address

--- frontend/lib/config/production.ts
+++ frontend/lib/config/production.ts
@@ -3,10 +3,15 @@ import type { ConfigInterface } from '~/lib/types/general.types';
 const config: ConfigInterface = {
   APP_URL: 'https://appointment.web3approved.com',
   API_BASE: 'https://appointment-api.web3approved.com',
-  // CHAIN_ID: 1284,
-  CHAIN_ID: 1287,
-  // CONTRACT_ADDRESS: '0x353eE349fd86c12D1f75f8d7d4190EF586D21b8e',
-  CONTRACT_ADDRESS: '0xBA15551dc8bc8c7063aFD0A3aB1b175889a530f8',
+  
+  // production
+  CHAIN_ID: 1284,
+  CONTRACT_ADDRESS: '0x805f08b1009EEd35b9777C6bB108264A054f31D5',
+  
+  // test
+  // CHAIN_ID: 1287,
+  // CONTRACT_ADDRESS: '0xBA15551dc8bc8c7063aFD0A3aB1b175889a530f8',
+
   METADATA_BASE_URI: null,
   METADATA_TOKEN: null,
 };
